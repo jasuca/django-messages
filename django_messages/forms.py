00d74e0bb66d60b81b29d3dc5b0afbc3faedd0cb
Gustavo Andres Morero <gus@gus.(none)>
2013-03-22 19:29:15 -0300

    naive datetimes correction

--- django_messages/forms.py
+++ django_messages/forms.py
@@ -1,4 +1,4 @@
-import datetime
+from django.utils import timezone
 from django import forms
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
@@ -42,7 +42,7 @@ class ComposeForm(forms.Form):
             )
             if parent_msg is not None:
                 msg.parent_msg = parent_msg
-                parent_msg.replied_at = datetime.datetime.now()
+                parent_msg.replied_at = timezone.now()
                 parent_msg.save()
             msg.save()
             message_list.append(msg)
